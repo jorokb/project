26ef9c0d604c06af19e228ce7f0924c50a004929
jorokb <54720856+jorokb@users.noreply.github.com>
2019-10-10 21:18:31 +0700

    Change view.py

--- PROJECT/PROJECTAPP/views.py
+++ PROJECT/PROJECTAPP/views.py
@@ -15,19 +15,31 @@ def index(request):
     return render(request, "PROJECTAPP/hi.html")
 
 def lpc(request):
-    return render(request, "PROJECTAPP/lpc.html")
+    resp = HttpResponse(content_type='application/pdf')
+    result = generate_pdf('PROJECTAPP/lpc.html', file_object=resp)
+    return result
 
 def lpn(request):
-    return render(request, "PROJECTAPP/lpn.html")
+    resp = HttpResponse(content_type='application/pdf')
+    result = generate_pdf('PROJECTAPP/lpn.html', file_object=resp)
+    return result
 
 def lpn2(request):
-    return render(request, "PROJECTAPP/lpn2.html")
+    resp = HttpResponse(content_type='application/pdf')
+    result = generate_pdf('PROJECTAPP/lpn2.html', file_object=resp)
+    return result
 
 def lpp(request):
-    return render(request, "PROJECTAPP/lpp.html")
+    resp = HttpResponse(content_type='application/pdf')
+    result = generate_pdf('PROJECTAPP/lpp.html', file_object=resp)
+    return result
 
 def pst(request):
-    return render(request, "PROJECTAPP/pst.html")
+    resp = HttpResponse(content_type='application/pdf')
+    result = generate_pdf('PROJECTAPP/pst.html', file_object=resp)
+    return result
 
 def pst2(request):
-    return render(request, "PROJECTAPP/pst2.html")
+    resp = HttpResponse(content_type='application/pdf')
+    result = generate_pdf('PROJECTAPP/pst2.html', file_object=resp)
+    return result
